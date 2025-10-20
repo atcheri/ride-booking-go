09c8aa841d14d5788a8d42c091fc6b2ccfee5ebf
Atsuhiro <atcheri@gmail.com>
2025-10-20 18:43:11 +0200

    fix: typo in the json payload parameter

--- services/api-gateway/dto.go
+++ services/api-gateway/dto.go
@@ -4,6 +4,6 @@ import "github.com/atcheri/ride-booking-go/shared/types"
 
 type tripPreviewRequest struct {
 	UserId      string           `json:"userID"`
-	PickUp      types.Coordinate `json:"pikcup"`
+	PickUp      types.Coordinate `json:"pickup"`
 	Destination types.Coordinate `json:"destination"`
 }
