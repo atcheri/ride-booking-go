a360f871290ca3e94bc1665301a9cdd9c2dcfca0
Atsuhiro <atcheri@gmail.com>
2025-10-22 14:32:12 +0200

    fix: move the osrm response type to the trip-service

--- services/api-gateway/dto.go
+++ services/api-gateway/dto.go
@@ -1,9 +1,26 @@
 package main
 
-import "github.com/atcheri/ride-booking-go/shared/types"
+import (
+	"github.com/atcheri/ride-booking-go/shared/types"
+	pb "github.com/atcheri/ride-booking-grpc-proto/golang/trip"
+)
 
 type tripPreviewRequest struct {
 	UserId      string           `json:"userID"`
 	PickUp      types.Coordinate `json:"pickup"`
 	Destination types.Coordinate `json:"destination"`
+}
+
+func (r *tripPreviewRequest) ToProto() *pb.PreviewTripRequest {
+	return &pb.PreviewTripRequest{
+		UserID: r.UserId,
+		StartLocation: &pb.Coordinate{
+			Latitude:  r.PickUp.Latitude,
+			Longitude: r.PickUp.Longitude,
+		},
+		EndLocation: &pb.Coordinate{
+			Latitude:  r.Destination.Latitude,
+			Longitude: r.Destination.Longitude,
+		},
+	}
 }
